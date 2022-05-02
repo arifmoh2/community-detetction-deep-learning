e2129d3a2d7e749092f37b7045e306651bda261a
Filip Ryzner <ryznerf@dhcp-10-31-74-28.dyn.mit.edu>
2022-05-02 15:46:54 -0400

    preparing results

--- Main_Code_Repo/Plotting.py
+++ Main_Code_Repo/Plotting.py
@@ -2,17 +2,18 @@ import networkx
 import networkx as nx
 import matplotlib.pyplot as plt
 from matplotlib.cm import get_cmap
-
-name = "Accent_r"
-
-cmap = get_cmap(name)
-colors = cmap.colors
+import numpy as np
 
 
 def graph_cluster_plot(g: networkx.Graph, partition:dict = None, title =  'Partition plot' ,seed = 0):
     pos = nx.spring_layout(g, seed=seed)
     if partition is None:
         partition = {node:i for i, cluster in enumerate(g.graph['partition']) for node in cluster}
+
+    name = "Accent_r"
+    cmap_len = len(np.unique(list(partition.values())))
+    cmap = get_cmap(name, cmap_len)
+    colors = cmap.colors
 
     nx.draw(g,
             pos,
@@ -25,8 +26,15 @@ def graph_cluster_plot(g: networkx.Graph, partition:dict = None, title =  'Parti
 
 def clustering_comparison_plot(g: networkx.Graph, partition_true:dict,
                                partition_predicted:dict ,seed = 0, node_size = 100):
+
     pos = nx.spring_layout(g, seed=seed)
     fig, axes = plt.subplots(1, 2, figsize=(15, 5))
+
+    # true color map
+    name = "Accent_r"
+    cmap_len = len(np.unique(list(partition_true.values())))
+    cmap = get_cmap(name, cmap_len)
+    colors = cmap.colors
     nx.draw(g,
             pos,
             node_size=node_size,
@@ -37,6 +45,10 @@ def clustering_comparison_plot(g: networkx.Graph, partition_true:dict,
             )
     axes[0].set_title('True')
 
+    # prediction color map
+    cmap_len = len(np.unique(list(partition_predicted.values())))
+    cmap = get_cmap(name, cmap_len)
+    colors = cmap.colors
     nx.draw(g,
             pos,
             node_size=node_size,
